43feb1a43c197b846dccfca1110df7a7737672dd
Matthew Hughes <matt.hughes@live.com>
2020-12-08 20:32:24 +1100

    Initial commit

---
+++ Assets/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NRKernal;
+using Eidetic.URack.Collection;
+
+public class TestController : MonoBehaviour
+{
+    public Mirage2x Mirage2x;
+
+    void Update()
+    {
+        // if (Mirage2x == null)
+        //     Mirage2x = GameObject.Find("Mirage2xInstance280").GetComponent<Mirage2x>();
+        // var handPos = NRInput.GetPosition();
+        // Mirage2x.PositionX = handPos.x;
+        // Mirage2x.PositionY = handPos.y;
+        // Mirage2x.PositionZ = handPos.z;
+    }
+}
